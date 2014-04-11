f1235a51118ff7f5557080a6aa8c6d2fdcd055b1
iAmMatthew <matthew.chi0416@icloud.com>
2014-04-11 17:33:44 +0000

    Changed the code to make me win all the time.

--- simple_rps.rb
+++ simple_rps.rb
@@ -1,4 +1,8 @@
-OUTCOMES = {"rock" =>{"rock" => "tie", "paper" => "Player 2", => "Player 1},
-            "paper" =>{},
-            "scissors" =>{}
+OUTCOMES = {"rock" =>{"rock" => "You Win", "paper" => "You Win","scissors" => "You Win"},
+            "paper" => {"paper" => "You Win", "rock" => "You Win", "scissors" => "You Win"},
+            "scissors" =>{"scissors" => "You Win", "rock"=> "You Win", "paper" => "You Win"}
             }
+computer_choice = ["rock", "paper", "scissors"].sample
+puts "Pick a choice: rock, paper, scissors"
+user_choice = gets.chomp
+puts OUTCOMES[user_choice][computer_choice]
